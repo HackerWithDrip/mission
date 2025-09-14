155c2e0b848194ae3b2f02fabe022632749416eb
Lionel Raseemela <lionel@Lionels-MacBook-Pro.local>
2025-09-15 00:12:03 +0200

    last testing before sleeping

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div style={{display:'flex',height:'100vh',alignItems:'center',justifyContent:'center',fontFamily:'Arial,Helvetica,sans-serif'}}>
-      <h1>Hello everyone! Welcome to Cloud!mgmmgmgmgmgmgmgm</h1>
+      <h1>Hello everyone! Welcome to Cloud!</h1>
     </div>
   );
 }
