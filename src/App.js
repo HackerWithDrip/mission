ba37debbd341a683ca230fb4efb0003fef105069
Lionel Raseemela <lionel@Lionels-MacBook-Pro.local>
2025-09-14 11:07:38 +0200

    created a simple website to display ‘welcome to cloud‘

---
+++ src/App.js
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div style={{display:'flex',height:'100vh',alignItems:'center',justifyContent:'center',fontFamily:'Arial,Helvetica,sans-serif'}}>
+      <h1>Hello everyone! Welcome to Cloud!</h1>
+    </div>
+  );
+}
